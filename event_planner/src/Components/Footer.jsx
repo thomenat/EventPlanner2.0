da7e24b9cba894ceb22d6fdc5188ffe51890b07e
˜nthome˜ <nataliathome@gmail.com>
2025-02-10 21:09:24 +0000

    Add event planner directory

---
+++ event_planner/src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Footer = () => {
+        return (
+            <footer>
+                <p>&copy; Event Planner Organization. All rights reserved.</p>
+            </footer>
+)}
+
+export default Footer
